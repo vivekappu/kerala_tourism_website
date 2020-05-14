bc42e28fc4135fe71eaf015de494f821b9be0d17
vivek <vivek.appu619@gmail.com>
2020-05-14 11:58:04 +0530

    validation added on sign in page

---
+++ signinvalidation.js
@@ -0,0 +1,45 @@
+let email=document.getElementById('login-email');
+let password=document.getElementById('login-password');
+let loginbtn=document.getElementById('btn-login');
+let emailtooltip=document.querySelector('.emailtooltip');
+let passwordtooltip=document.querySelector('.passwordtooltip');
+const emailRegex = /^[^@]+@[^@.]+\.[a-z]+$/i;
+
+function Show(show, Element) {
+    if (show) {
+        Element.style.display = "inherit";
+    }
+    else {
+        Element.style.display = "none";
+    }
+}
+function isEmpty(input) {
+    return !input.value.length;
+}
+function Validation(text, regex) {
+    return regex.test(text);
+}
+function createListener(validator, regex) {
+    console.log(regex);
+    return e => {
+        const text = e.target.value;
+        const valid = validator(text, regex);
+        const show = text !== "" && !valid;
+        const showelement = e.target.nextElementSibling;
+        Show(show, showelement);
+       
+    }
+}
+
+email.addEventListener("input", createListener(Validation, emailRegex));
+password.addEventListener("input",()=>{
+    Show(false,passwordtooltip);
+})
+loginbtn.addEventListener("click",()=>{
+    if(isEmpty(email)){
+        Show(true,emailtooltip);
+    }
+    if(isEmpty(password)){
+        Show(true,passwordtooltip)
+    }
+});
